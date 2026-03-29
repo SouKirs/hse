55d234a526a60c4edc0c50ea0e803c3e78cdbffe
Babenko Polina <llnataliall@mail.ru>
2026-03-29 22:27:57 +0500

    Подготовка проекта для Render (requirements + запуск сервера)

--- main.py
+++ main.py
@@ -1071,3 +1071,9 @@ def read_root():
     return {"message": "Привет! FastAPI работает!"}
 
 
+# ---------- Запуск сервера ----------
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Render передаст свой порт, локально используем 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
